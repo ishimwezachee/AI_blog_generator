5a3cd03efc34017982c97cbc7bcb892249d393fe
ishimwezachee <zacheeishimwe@gmail.com>
2023-02-14 17:33:14 +0200

    add api key

--- blog_ai_generator.py
+++ blog_ai_generator.py
@@ -1,8 +1,11 @@
 
 import os
 import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
+import config
+openai.api_key = os.getenv(config.OPENAI_API_KEY)
 openai.Embedding.create(
   model="text-embedding-ada-002",
   input="The food was delicious and the waiter..."
 )
+
+print()
